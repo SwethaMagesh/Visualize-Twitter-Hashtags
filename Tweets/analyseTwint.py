aa3d20ace24c0726bf52bbd5e51af6d2948600b8
SwethaMagesh <swethamag.2000@gmail.com>
2021-03-18 12:56:55 +0530

    checked parse n analyse

---
+++ Tweets/analyseTwint.py
@@ -0,0 +1,134 @@
+#Import all the needed libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import seaborn as sns
+import re
+import collections
+from wordcloud import WordCloud
+
+
+#Reading the raw data collected from the Twitter Streaming API using #Tweepy. 
+tweets_data = []
+tweets_text=[]
+tweets_data_path = 'IPLAuction2021.csv'
+with open('iplauction2.txt','r') as file:
+    
+    for line in file.read().split('\n'):
+        field = line.split(' ')
+        tweets=dict()
+        if len(field)<5:
+            continue
+        tweets['id']=field[0]
+        tweets['date']=field[1]
+        tweets['time']=field[2]
+        temp=' '.join(field[4:])
+        #temp=temp.replace(r'[a-zA-Z0-9]','')
+        tweets['text']=temp
+        tweets_text.append(temp)
+        tweets_data.append(tweets)
+
+
+
+print("The total number of Tweets is:",len(tweets_data))
+
+
+#To see the most used hashtags.
+hashtags = []
+hashtag_pattern = re.compile(r"#[a-zA-Z]+")
+hashtag_matches=[]
+for text in tweets_text:
+    hashtag_matches.append(hashtag_pattern.findall(text))
+hashtag_dict = {}
+for match in hashtag_matches:
+    for singlematch in match:
+        if singlematch not in hashtag_dict.keys():
+            hashtag_dict[singlematch] = 1
+        else:
+            hashtag_dict[singlematch] = hashtag_dict[singlematch]+1
+
+
+
+#Making a list of the most used hashtags and their values
+hashtag_ordered_list =sorted(hashtag_dict.items(), key=lambda x:x[1])
+hashtag_ordered_list = hashtag_ordered_list[::-1]
+#Separating the hashtags and their values into two different lists
+hashtag_ordered_values = []
+hashtag_ordered_keys = []
+#Pick the 20 most used hashtags to plot
+for item in hashtag_ordered_list[0:20]:
+    hashtag_ordered_keys.append(item[0])
+    hashtag_ordered_values.append(item[1])
+#Plotting a graph with the most used hashtags
+fig, ax = plt.subplots(figsize = (12,12))
+y_pos = np.arange(len(hashtag_ordered_keys))
+ax.barh(y_pos ,list(hashtag_ordered_values)[::-1], align='center', color = 'green', edgecolor = 'black', linewidth=1)
+ax.set_yticks(y_pos)
+ax.set_yticklabels(list(hashtag_ordered_keys)[::-1])
+ax.set_xlabel("No of appereances")
+ax.set_title("Most used #hashtags", fontsize = 20)
+plt.tight_layout(pad=3)
+plt.show()
+
+#Make a wordcloud plot of the most used hashtags, for this we need a #dictionary 
+#where the keys are the words and the values are the number of #appearances
+hashtag_ordered_dict = {}
+for item in hashtag_ordered_list[0:20]:
+    hashtag_ordered_dict[item[0]] = item[1]
+wordcloud = WordCloud(width=1000, height=1000, random_state=21, max_font_size=200, background_color = 'white').generate_from_frequencies(hashtag_ordered_dict)
+plt.figure(figsize=(15, 10))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis('off')
+
+plt.show()
+
+
+#Now we will do the same with the mentions:
+mentions = []
+mention_pattern = re.compile(r"@[a-zA-Z_]+")
+mention_matches=[]
+for text in tweets_text:
+    mention_matches.append(mention_pattern.findall(text))
+
+mentions_dict = {}
+for match in mention_matches:
+    for singlematch in match:
+        if singlematch not in mentions_dict.keys():
+            mentions_dict[singlematch] = 1
+        else:
+            mentions_dict[singlematch] = mentions_dict[singlematch]+1
+
+#Create an ordered list of tuples with the most mentioned users and #the number of times they have been mentioned
+mentions_ordered_list =sorted(mentions_dict.items(), key=lambda x:x[1])
+mentions_ordered_list = mentions_ordered_list[::-1]
+#Pick the 20 top mentioned users to plot and separate the previous #list into two list: one with the users and one with the values
+mentions_ordered_values = []
+mentions_ordered_keys = []
+for item in mentions_ordered_list[0:20]:
+    mentions_ordered_keys.append(item[0])
+    mentions_ordered_values.append(item[1])
+
+
+fig, ax = plt.subplots(figsize = (12,12))
+y_pos = np.arange(len(mentions_ordered_values))
+ax.barh(y_pos ,list(mentions_ordered_values)[::-1], align='center', color = 'yellow', edgecolor = 'black', linewidth=1)
+ax.set_yticks(y_pos )
+ax.set_yticklabels(list(mentions_ordered_keys)[::-1])
+ax.set_xlabel("No of mentions")
+ax.set_title("Most mentioned accounts", fontsize = 20)
+
+plt.show()
+
+#Make a wordcloud representation for the most mentioned accounts too
+mentions_ordered_dict = {}
+for item in mentions_ordered_list[0:20]:
+    mentions_ordered_dict[item[0]] = item[1]
+wordcloud = WordCloud(width=1000, height=1000, random_state=21, max_font_size=200, background_color = 'white').generate_from_frequencies(mentions_ordered_dict)
+plt.figure(figsize=(15, 10))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis('off')
+
+plt.show()
+
+
