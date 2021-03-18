dc9360da54b87990fd1caddb8fac9f7c1255ed81
SwethaMagesh <swethamag.2000@gmail.com>
2021-03-18 12:55:11 +0530

    another search output

--- Free/twintsample.py
+++ Free/twintsample.py
@@ -2,9 +2,6 @@ import twint
 
 # Configure
 c = twint.Config()
-c.Search = "#IPLAuction2021"
-c.since="2021-02-17"
-c.until="2021-02-19"
-
+c.Search = "(#IPLAuction2021 OR #IPL2021 OR #CSK OR #MI OR #IPLAuctions OR #RCB) until:2021-02-19 since:2021-02-17"
 # Run
 twint.run.Search(c)
