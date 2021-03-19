5c6e6e2fd83d3c3f758a8b1fffb6342c166c3d33
SwethaMagesh <swethamag.2000@gmail.com>
2021-03-19 21:54:15 +0530

    search strings addded

--- Free/twintsample.py
+++
@@ -1,7 +0,0 @@
-import twint
-
-# Configure
-c = twint.Config()
-c.Search = "-#IPLAuction2021 -#IPL2021 -#CSK -#MI -#RCB -#IPLAuctions (#IPlAuction2021live OR #Yellove OR #WhistlePodu OR #MumbaiIndians OR #YehHaiNayiDilli OR #WeRoarTogether OR #OrangeArmy OR #PunjabKings OR #RisersRetained OR #Sachin OR #ArjunTendulkar OR #SachinBaby OR #Pujara OR #GlennMaxwell) until:2021-02-19 since:2021-02-17"
-# Run
-twint.run.Search(c)
